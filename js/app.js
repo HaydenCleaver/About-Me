509c1163028dc7434de3f6237ab2ce31ed0691fd
Hayden Cleaver <rexcleaver90@gmail.com>
2022-06-07 21:19:22 -0400

    minor html corrections; finished with js

--- js/app.js
+++ js/app.js
@@ -9,51 +9,60 @@ function fiveQuestions(){
   let result = question.toLowerCase();
 
   if ( result === 'yes' || result === 'y'){
-    console.log('That\'s correct!');
-
+    // console.log('That\'s correct!');
+    alert('That\'s correct!');
   } else{
-    console.log('That\'s incorrect!');
+    // console.log('That\'s incorrect!');
+    alert('That\'s incorrect!');
   }
 
   question = prompt('Is my favorite season summer?');
   result = question.toLowerCase();
 
   if ( result === 'no' || result === 'n'){
-    console.log('That\'s correct!');
+    // console.log('That\'s correct!');
+    alert('That\'s correct!');
 
   } else{
-    console.log('That\'s incorrect!');
+    // console.log('That\'s incorrect!');
+    alert('That\'s incorrect!');
   }
 
   question = prompt('Do I like to kayak?');
   result = question.toLowerCase();
 
   if ( result === 'yes' || result === 'y'){
-    console.log('That\'s correct!');
+    // console.log('That\'s correct!');
+    alert('That\'s correct!');
 
   } else{
-    console.log('That\'s incorrect!');
+    // console.log('That\'s incorrect!');
+    alert('That\'s incorrect!');
   }
 
   question = prompt('Do I have a college degree?');
   result = question.toLowerCase();
 
   if ( result === 'no' || result === 'n'){
-    console.log('That\'s correct!');
+    // console.log('That\'s correct!');
+    alert('That\'s correct!');
 
   } else{
-    console.log('That\'s incorrect!');
+    // console.log('That\'s incorrect!');
+    alert('That\'s incorrect!');
   }
 
   question = prompt('Do I want to move to a different country?');
   result = question.toLowerCase();
 
   if ( result === 'yes' || result === 'y'){
-    console.log('That\'s correct!');
+    //console.log('That\'s correct!');
+    alert('That\'s correct!');
     alert('Thanks for playing my game ' + user + '!');
 
   } else{
-    console.log('That\'s incorrect!');
+    //console.log('That\'s incorrect!');
+    alert('That\'s incorrect!');
     alert('Thanks for playing my game ' + user + '!');
   }
 
