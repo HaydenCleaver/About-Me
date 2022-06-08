1891523f7c531976edef365f69b06aa9a3f27085
Hayden Cleaver <rexcleaver90@gmail.com>
2022-06-08 00:07:04 -0400

    initial css commit; fixing more html

--- README.md
+++ README.md
@@ -2,16 +2,18 @@
 
 ## About Me
 
-I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+
 
 ### Author: Hayden Cleaver
+Website that gives a little background on myself and my work history/education. Includes a guessing game button that lets the user interact with the information provided.
 
 ### Links and Resources
 
-submission PR \
-Any Links you used as reference
+[Basic HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)\
+[Additional HTML Resources](https://developer.mozilla.org/en-US/docs/Web/HTML)
 
 ### Reflections and Comments
 
-Consider including the answers to your daily journal and submission questions here
-This is also a good place to reflect on the tools and resources used and learned
+I'm pretty sure that I could use an array to make my code way more effecient.  I'll mess around with it tomorrow morning if I have time.
+
+I also learned to be very careful about typing out the file path for using external docs.  I think I avoided the specific pitfall that Jacob and AJ mentioned, but I added an extra backslash at the front of the path, so github was trying to pull the file from a nonexistant path.  It took me a good 10 minutes and a lucky StackOverflow query to figure out why my Javascript wasn't working on the Github site.
--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@
 let user = prompt('Please enter your name: ');
 alert('Welcome ' + user + '!');
 
-function fiveQuestions(){
+function fiveQuestions() {
 
   let question = prompt('Was I born in Baltimore?');
   let result = question.toLowerCase();
 
-  if ( result === 'yes' || result === 'y'){
+  if (result === 'yes' || result === 'y') {
     // console.log('That\'s correct!');
     alert('That\'s correct!');
-  } else{
+  } else {
     // console.log('That\'s incorrect!');
     alert('That\'s incorrect!');
   }
@@ -19,11 +19,11 @@ function fiveQuestions(){
   question = prompt('Is my favorite season summer?');
   result = question.toLowerCase();
 
-  if ( result === 'no' || result === 'n'){
+  if (result === 'no' || result === 'n') {
     // console.log('That\'s correct!');
     alert('That\'s correct!');
 
-  } else{
+  } else {
     // console.log('That\'s incorrect!');
     alert('That\'s incorrect!');
   }
@@ -31,11 +31,11 @@ function fiveQuestions(){
   question = prompt('Do I like to kayak?');
   result = question.toLowerCase();
 
-  if ( result === 'yes' || result === 'y'){
+  if (result === 'yes' || result === 'y') {
     // console.log('That\'s correct!');
     alert('That\'s correct!');
 
-  } else{
+  } else {
     // console.log('That\'s incorrect!');
     alert('That\'s incorrect!');
   }
@@ -43,11 +43,11 @@ function fiveQuestions(){
   question = prompt('Do I have a college degree?');
   result = question.toLowerCase();
 
-  if ( result === 'no' || result === 'n'){
+  if (result === 'no' || result === 'n') {
     // console.log('That\'s correct!');
     alert('That\'s correct!');
 
-  } else{
+  } else {
     // console.log('That\'s incorrect!');
     alert('That\'s incorrect!');
   }
@@ -55,12 +55,12 @@ function fiveQuestions(){
   question = prompt('Do I want to move to a different country?');
   result = question.toLowerCase();
 
-  if ( result === 'yes' || result === 'y'){
+  if (result === 'yes' || result === 'y') {
     //console.log('That\'s correct!');
     alert('That\'s correct!');
     alert('Thanks for playing my game ' + user + '!');
 
-  } else{
+  } else {
     //console.log('That\'s incorrect!');
     alert('That\'s incorrect!');
     alert('Thanks for playing my game ' + user + '!');
