b3e7b270fa5ebfefa7b1120288b23b009ed23a9a
Hayden Cleaver <rexcleaver90@gmail.com>
2022-06-07 21:07:13 -0400

    actually finished html text; initial js commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+let user = prompt('Please enter your name: ');
+alert('Welcome ' + user + '!');
+
+function fiveQuestions(){
+
+  let question = prompt('Was I born in Baltimore?');
+  let result = question.toLowerCase();
+
+  if ( result === 'yes' || result === 'y'){
+    console.log('That\'s correct!');
+
+  } else{
+    console.log('That\'s incorrect!');
+  }
+
+  question = prompt('Is my favorite season summer?');
+  result = question.toLowerCase();
+
+  if ( result === 'no' || result === 'n'){
+    console.log('That\'s correct!');
+
+  } else{
+    console.log('That\'s incorrect!');
+  }
+
+  question = prompt('Do I like to kayak?');
+  result = question.toLowerCase();
+
+  if ( result === 'yes' || result === 'y'){
+    console.log('That\'s correct!');
+
+  } else{
+    console.log('That\'s incorrect!');
+  }
+
+  question = prompt('Do I have a college degree?');
+  result = question.toLowerCase();
+
+  if ( result === 'no' || result === 'n'){
+    console.log('That\'s correct!');
+
+  } else{
+    console.log('That\'s incorrect!');
+  }
+
+  question = prompt('Do I want to move to a different country?');
+  result = question.toLowerCase();
+
+  if ( result === 'yes' || result === 'y'){
+    console.log('That\'s correct!');
+    alert('Thanks for playing my game ' + user + '!');
+
+  } else{
+    console.log('That\'s incorrect!');
+    alert('Thanks for playing my game ' + user + '!');
+  }
+
+}
