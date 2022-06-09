79da9b8fedb4c2185c605a16a81c86836a3c0366
Hayden Cleaver <rexcleaver90@gmail.com>
2022-06-08 21:50:30 -0400

    fulfilled base lab3 requirements

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
 let user = prompt('Please enter your name: ');
 alert('Welcome ' + user + '!');
 
+let topThree = ['big sur', 'grandfather mountain', 'aiea trail'];
+let correct = 0;
+let numAnswer = Math.floor(Math.random() * 10);
+
 function fiveQuestions() {
 
   let question = prompt('Was I born in Baltimore?');
@@ -11,6 +15,8 @@ function fiveQuestions() {
   if (result === 'yes' || result === 'y') {
     // console.log('That\'s correct!');
     alert('That\'s correct!');
+    correct++;
+
   } else {
     // console.log('That\'s incorrect!');
     alert('That\'s incorrect!');
@@ -22,6 +28,7 @@ function fiveQuestions() {
   if (result === 'no' || result === 'n') {
     // console.log('That\'s correct!');
     alert('That\'s correct!');
+    correct++;
 
   } else {
     // console.log('That\'s incorrect!');
@@ -34,6 +41,7 @@ function fiveQuestions() {
   if (result === 'yes' || result === 'y') {
     // console.log('That\'s correct!');
     alert('That\'s correct!');
+    correct++;
 
   } else {
     // console.log('That\'s incorrect!');
@@ -46,6 +54,7 @@ function fiveQuestions() {
   if (result === 'no' || result === 'n') {
     // console.log('That\'s correct!');
     alert('That\'s correct!');
+    correct++;
 
   } else {
     // console.log('That\'s incorrect!');
@@ -58,12 +67,69 @@ function fiveQuestions() {
   if (result === 'yes' || result === 'y') {
     //console.log('That\'s correct!');
     alert('That\'s correct!');
-    alert('Thanks for playing my game ' + user + '!');
+    correct++;
 
   } else {
     //console.log('That\'s incorrect!');
     alert('That\'s incorrect!');
-    alert('Thanks for playing my game ' + user + '!');
+  }
+
+  for (let i = 0; i < 4; i++) {
+
+    question = prompt('Guess a number 0-10. You get 4 chances.');
+    result = Number(question);
+
+    if (result === numAnswer) {
+      alert('That is correct!');
+      correct++;
+      break;
+    }
+
+    else if (result < numAnswer) {
+      alert('Too low! You have ' + (4 - (i + 1)) + ' chances left.');
+    }
+
+    else if (result > numAnswer) {
+      alert('Too high! You have ' + (4 - (i + 1)) + ' chances left.');
+    }
+
+    else {
+      alert('Invalid answer. You have ' + (4 - (i + 1)) + ' chances left.');
+    }
+  }
+
+  for (let i = 0; i < 6; i++) {
+    question = prompt('What is one of my top three favorite trails?');
+    result = question.toLowerCase();
+
+    for (let n = 0; n < topThree.length; n++) {
+      if (result === topThree[n]) {
+        alert('That is correct!');
+        alert('These are all of the correct answers: ' + topThree);
+        correct++;
+
+        if (correct === 7) {
+          alert('You got all 7 correct.  Good job ' + user + '!');
+          correct = 0;
+          return;
+        }
+
+        else {
+          alert('You got ' + correct + ' answers correct. Try to get all 7!');
+          alert('Thanks for playing my game ' + user + '!');
+          correct = 0;
+          return;
+        }
+      }
+
+      // else {
+      //   alert('That is incorrect! You have ' + (6-(i+1)) + ' guesses left.');
+      //   // break;
+
+      // }
+
+    }
+    alert('That is incorrect! You have ' + (6 - (i + 1)) + ' guesses left.');
   }
 
 }
